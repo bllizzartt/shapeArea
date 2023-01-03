4149a220367e054d5ab4658614dc9be05361b4ec
Chase_Haynes <chase.haynez@gmail.com>
2023-01-03 10:58:02 -0500

    shapeArea
    finished

---
+++ shapeArea.js
@@ -0,0 +1,23 @@
+// find the area of the polygon
+// problem 1
+// initate  let variable "n" equal to 2
+let n = 2;
+// intiate a function 
+function shapeArea(n) {
+    return n*n + (n-1)*(n-1);
+    }
+    
+    let result =  n*n + (n-1)*(n-1);
+console.log(result);
+
+
+// problme 2
+// initate  let variable "n1" equal to 3
+let n1 = 3;
+// initiate a function
+function shapeArea(n1) {
+    return n1*n1 + (n1-1)*(n1-1);
+}
+
+let result1 = n1*n1 + (n1-1)*(n1-1);
+console.log(result1);
